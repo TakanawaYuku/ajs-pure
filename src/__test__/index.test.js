900bbf45f275d9ab3acd50d88ab5c7959310213d
TakanawaYuku <yukutakanawa@gmail.com>
2023-02-19 11:53:37 +0300

    hm code add files js

---
+++ src/__test__/index.test.js
@@ -0,0 +1,10 @@
+import getHealthPerson from '../index';
+
+test.each([
+  [90, 'healthy'],
+  [44, 'wounded'],
+  [10, 'critical'],
+])('health status for %i HP', (health, expected) => {
+  const result = getHealthPerson({ name: 'Маг', health });
+  expect(result).toBe(expected);
+});
---
+++ src/index.js
@@ -0,0 +1,22 @@
+export default function getHealthPerson(player) {
+    let health = '';
+    if (player.health >= 50 && player.health <= 100) {
+      health = 'healthy';
+    } if (player.health < 50 && player.health >= 15) {
+      health = 'wounded';
+    } if (player.health < 15) {
+      health = 'critical';
+    } else {
+      health = 'incorrect result';
+    }
+
+    return health;
+  }
+
+  const person = {
+    name: 'Маг',
+    health: 14,
+  };
+
+  getHealthPerson(person);
+  // console.log(getHealthPerson(person)); Для проверки что код работает
