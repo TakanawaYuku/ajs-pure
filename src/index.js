42e3abfedcd260835eb4e58a6c16f095b8a58a4d
TakanawaYuku <yukutakanawa@gmail.com>
2023-02-19 13:57:27 +0300

    CRLF => LF

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 export default function getHealthPerson(player) {
-    let health = '';
-    if (player.health >= 50 && player.health <= 100) {
-      health = 'healthy';
-    } if (player.health < 50 && player.health >= 15) {
-      health = 'wounded';
-    } if (player.health < 15) {
-      health = 'critical';
-    } else {
-      health = 'incorrect result';
-    }
-
-    return health;
+  let health = '';
+  if (player.health >= 50 && player.health <= 100) {
+    health = 'healthy';
+  } if (player.health < 50 && player.health >= 15) {
+    health = 'wounded';
+  } if (player.health < 15) {
+    health = 'critical';
+  } else {
+    health = 'incorrect result';
   }
 
-  const person = {
-    name: 'Маг',
-    health: 14,
-  };
+  return health;
+}
 
-  getHealthPerson(person);
-  // console.log(getHealthPerson(person)); Для проверки что код работает
+const person = {
+  name: 'Маг',
+  health: 14,
+};
+
+getHealthPerson(person);
+// console.log(getHealthPerson(person)); Для проверки что код работает
